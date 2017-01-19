b25e01a78d74d0e6a2cca19924d8319b55070b0d
Tzu Ming Harry Hsu <harry19930924@yahoo.com.tw>
2017-01-19 08:18:13 +0800

    new

--- main.py
+++ main.py
@@ -2,32 +2,22 @@
 
 import bs4 
 import flask
+import flask_bootstrap
 import util
 
 app = flask.Flask(__name__)
+flask_bootstrap.Bootstrap(app)
+
 eclient = util.Eclient(server='http://10.167.131.227')
 eclient.login()
 
-@app.route('/rcv')
+@app.route('/receive')
 def receive():
-    soup = bs4.BeautifulSoup('', 'html.parser')
-    soup.append(soup.new_tag('ol'))
-
     documents = eclient.receive(
         startDate='106-01-01',
         endDate='106-01-10',
     )
 
-    for document in documents:
-        li = soup.new_tag('li')
-        #import pdb; pdb.set_trace()
-        li.string = unicode(document)
-        soup.ol.append(li)
-
-    eclient.receive_detail(documents[0])
-
-    #print(unicode(soup))
-    return flask.make_response(str(soup))
+    return flask.render_template('receive.html', documents=documents)
 
 app.run(host='0.0.0.0', port=1234)
-#receive()   
--- util.py
+++ util.py
@@ -3,51 +3,44 @@
 import bs4
 import datetime
 import itertools
+import pypyodbc
 import requests
+import sqlite3
 import urlparse
 
 class Document(object):
-    def __str__(self):
-        if self.attachments is not None:
-            attachment_str = u'附件: {}\n'.format(', '.join(self.attachments.keys()))
-        else:
-            attachment_str = u''
-
-        return (
-            u'{document.date:%Y-%m-%d} #{document.serial_no}\n'
-            u'{document.source_word}字第{document.source_no}號\n'
-            u'{document.subject}\n'
-            u'{attachment_str}\n'
-        ).format(
-            document=self,
-            attachment_str=attachment_str,
-        )
-
-    def __repr__(self):
-        return (
-            u'{document.date:%Y-%m-%d} {document.source_word}字第{document.source_no}號'
-        ).format(document=self)
-
     def __init__(self,
-                 source_name=None,
-                 source_word=None,
-                 source_no=None,
-                 date=None,
-                 serial_no=None,
-                 subject=None,
-                 dilistid=None,
-                 attachments=None,
-                 **kwargs):
+                 eclient,
+                 source_name,
+                 source_word,
+                 source_no,
+                 date,
+                 subject,
+                 dilistid):
 
+        self.eclient = eclient
         self.source_name = source_name
         self.source_word = source_word
         self.source_no = source_no
         self.date = date
-        self.serial_no = serial_no
         self.subject = subject
         self.dilistid = dilistid
-        self.attachments = attachments
-        self.__dict__.update(kwargs)
+
+    def receive(self):
+        params = {
+            'menuCode': 'RECVQRY',
+            'detail': 'showdetail',
+            'dilistid': self.dilistid,
+            'listid': '',
+        }
+
+        r = self.sess.get(self.eclient.route('webeClient/main.php'), params=params)
+        soup = bs4.BeautifulSoup(r.content, 'html.parser')
+        as_ = soup.select('table#Table1 a')
+
+        self.attachments = {}
+        for a_ in as_[1:]:
+            self.attachments[a_.string] = a_['href']
 
 
 class Eclient(object):
@@ -119,6 +112,7 @@ class Eclient(object):
             tds = tr0.find_all('td')
 
             document = Document(
+                eclient=self,
                 source_name=tds[4].contents[2].string,
                 source_word=tds[5].string,
                 source_no=int(tds[6].contents[1].string),
@@ -130,18 +124,35 @@ class Eclient(object):
 
         return documents
 
-    def receive_detail(self, document):
-        params = {
-            'menuCode': 'RECVQRY',
-            'detail': 'showdetail',
-            'dilistid': document.dilistid,
-            'listid': '',
-        }
 
-        r = self.sess.get(self.route('webeClient/main.php'), params=params)
-        soup = bs4.BeautifulSoup(r.content, 'html.parser')
-        as_ = soup.select('table#Table1 a')
+class Config(object):
+    def __init__(self):
+        pass
+        
 
-        document.attachments = {}
-        for a_ in as_[1:]:
-            document.attachments[a_.string] = a_['href']
+class Database(object):
+    def __init__(self, 
+                 driver=u'{SQL Server Native Client 11.0}',
+                 server=u'隊本部收發\SQLEXPRESS',
+                 database=u'YMM_POLICE',
+                 uid=u'sa',
+                 pwd=u'twntfs@cloud'):
+
+        self.conn = pypyodbc.connect(
+            driver=driver,
+            server=server,
+            database=database,
+            uid=uid,
+            pwd=pwd,
+        )
+
+        cur = conn.cursor()
+
+        cur.execute((
+            'select top 10 * '
+            'from dbo.archive as x '
+            'where x.secret = ?'
+        ), (u'1 普通',))
+
+        for row in cur.fetchall():
+            print(row['receive_no'])
