736aaaea70365ba6310a0dfabfb40d0d34fa7e77
stmharry <harry19930924@gmail.com>
2017-01-27 17:43:44 +0800

    a bit refactoring

--- main.py
+++ main.py
@@ -1,110 +1,57 @@
 # -*- coding: utf-8 -*-
 
-import bs4 
+import datetime
 import flask
 import flask_bootstrap
-import pandas as pd
-import util
+
+from util import Document, eClient, Connection
 
 app = flask.Flask(__name__)
 flask_bootstrap.Bootstrap(app)
 
-eclient = util.eClient(server='http://10.167.131.227')
-eclient.login()
+eclient = eClient(
+    server='http://10.167.131.227',
+    userid='admin',
+    passwd='admin_123',
+)
 
-connection = util.Connection(
-    driver=u'{SQL Server Native Client 11.0}',
-    server=u'10.167.131.227,1433',
-    database=u'YMM_POLICE',
-    uid=u'sa',
-    pwd=u'twntfs@cloud',
+connection = Connection(
+    driver='{SQL Server Native Client 11.0}',
+    server='10.167.131.227,1433',
+    database='YMM_POLICE',
+    uid='sa',
+    pwd='twntfs@cloud',
     unicode_results=True,
 )
 
-# TODO: tidy up this part
-# ---
-
-conductors = connection.select(
-    from_='conductor', 
-    fields=['user_nm'],
+converter = Converter(
+    connection=connetion,
 )
-
-secrets = connection.select(
-    from_='secret',
-    fields=['code_no', 'code_nm'],
-)
-secrets.code_no = secrets.code_no.map(unicode.strip)
-secret_default = secrets[secrets.code_no == '1'].iloc[0]
-
-papers = connection.select(
-    from_='paper',
-    fields=['code_no', 'code_nm'],
-)
-papers.code_no = papers.code_no.map(unicode.strip)
-
-books = connection.select(
-    from_='book',
-    fields=['code_no', 'code_nm'],
-)
-books.code_no = books.code_no.map(unicode.strip)
-book_default = books[books.code_no == '1'].iloc[0]
-
-speed_default = u'普通件'
-archive_no_default = 1
-process_days_default = 0.5
-measure_default = u'頁'
-user_default = u'系統管理員'
-# ---
 
 @app.route('/receive', methods=['GET', 'POST'])
 def receive():
     alerts = []
 
+    now = datetime.datetime.now()
     documents = eclient.receive(
-        #startDate='106-01-01',
+        # start_date='106-01-01',
     )
 
     if flask.request.method == 'POST':
-        dilistid = int(flask.request.form['dilistid'])
+        id_ = int(flask.request.form['id'])
         conductor = flask.request.form['conductor']
 
-        document = [document for document in documents if document.dilistid == dilistid]
-        assert len(document) == 1
-        document = document[0]
-        document.receive()
-    
-        archives = connection.select(
-            from_='archive', 
-            fields=['sno', 'receive_no'],
-            wheres=['Year(receive_date) = {}'.format(util.now().year)],
-            order_bys=['sno desc'],
-        )
+        new_documents = [
+            eclient.receive_detail(document)
+            for document in documents
+            if document.id_ == id_
+        ]
+        new_documents[0].user_nm = conductor
 
-        now = util.now()
-        field_values = {
-            'ymm_year': now.year, 
-            'sno': max(archives.sno.tolist() or [0]) + 1,
-            'receive_no': max(archives.receive_no.map(int).tolist() or [0]) + 1,
-            'receive_date': '{:%Y/%m/%d}'.format(now),
-            'secret': u'{secret.code_no} {secret.code_nm}'.format(secret=secret_default),
-            'source': document.source,
-            'source_no': u'{document.source_word}字第{document.source_no}號'.format(document=document),
-            'paper': u'{paper.code_no} {paper.code_nm}'.format(paper=papers[papers.code_nm.map(unicode.strip) == document.paper].iloc[0]),
-            'book': u'{book.code_no} {book.code_nm}'.format(book=book_default),
-            'speed': speed_default,
-            'user_nm': conductor,
-            'subject': document.subject,
-            'archive_no': archive_no_default,
-            'process_days': process_days_default,
-            'measure': measure_default,
-            'ymm_month': now.month,
-            'ymm_user': user_default,
-        }
-
+        new_archives = converter.to_archives(new_documents)
         connection.insert(
+            new_archives,
             into='archive',
-            fields=field_values.keys(),
-            values=field_values.values(),
         )
 
     return flask.render_template(
--- util.py
+++ util.py
@@ -3,6 +3,7 @@
 import bs4
 import datetime
 import itertools
+import numpy as np
 import pandas as pd
 import pypyodbc
 import requests
@@ -10,113 +11,107 @@ import urlparse
 
 pd.set_option('display.unicode.east_asian_width', True)
 
-def now():
-    return datetime.datetime.now()
-
 
 class Document(object):
     def __init__(self,
-                 eclient,
+                 id_,
                  checked,
                  source,
-                 source_word,
                  source_no,
-                 date,
                  receiver,
+                 receive_datetime,
                  subject,
-                 dilistid):
+                 secret_nm=None,  # TODO
+                 paper_nm=None,  # TODO
+                 book_nm=None,  # TODO
+                 speed_nm=None,
+                 attachments=None,
+                 user_nm=None):
 
+        self.id_ = id_
         self.checked = checked
-        self.eclient = eclient
         self.source = source
-        self.source_word = source_word
         self.source_no = source_no
-        self.date = date
         self.receiver = receiver
+        self.receive_datetime = receive_datetime
         self.subject = subject
-        self.dilistid = dilistid
 
-    def receive(self):
-        params = {
-            'menuCode': 'RECVQRY',
-            'detail': 'showdetail',
-            'dilistid': self.dilistid,
-            'listid': '',
-        }
-
-        r = self.eclient.get('webeClient/main.php', params=params)
-        soup = bs4.BeautifulSoup(r.content, 'html.parser')
-        table = soup.find('table', id='Table1')
-        trs = table.find_all('tr')
-
-        tds = trs[1].find_all('td')
-        self.paper = tds[3].string
-
-        as_ = trs[4].find_all('a')
-        self.attachments = {}
-        for a_ in as_[1:]:
-            self.attachments[a_.string] = a_['href']
+        self.secret_nm = secret_nm
+        self.paper_nm = paper_nm
+        self.book_nm = book_nm
+        self.speed_nm = speed_nm
+        self.attachments = attachments
+        self.user_nm = user_nm
 
 
 class eClient(requests.Session):
-    def __init__(self, 
-                 server='http://localhost:80'):
+    def __init__(self,
+                 server,
+                 userid,
+                 passwd):
 
         super(eClient, self).__init__()
-        self.headers.update({'referer': ''})
         self.server = server
+
+        self.headers.update({'referer': ''})
+        self.post(
+            'webeClient/menu.php',
+            data={
+                'login': 1,
+                'userid': userid,
+                'passwd': passwd,
+            },
+        )
 
     def route(self, url):
         return urlparse.urljoin(self.server, url)
-        
+
     def get(self, url, *args, **kwargs):
         return super(eClient, self).get(self.route(url), *args, **kwargs)
 
     def post(self, url, *args, **kwargs):
         return super(eClient, self).post(self.route(url), *args, **kwargs)
 
-    def login(self, 
-              userid='admin',
-              passwd='admin_123'):
+    def receive(self,
+                source_word='',
+                source_number='',
+                source_id='',
+                source='',
+                subject='',
+                full_compare=0,
+                query_time='create_time',
+                start_date=None,
+                start_hour=0,
+                end_date=None,
+                end_hour=23):
 
-        self.post(
-            'webeClient/menu.php',
-            data={
-                'login': '1',
-                'userid': userid,
-                'passwd': passwd,
-            },
-        )
-
-    def receive(self, **kwargs):
-        now_ = now()
+        now = datetime.datetime.now()
         now_str = '{:d}-{:02d}-{:02d}'.format(
-            now_.year - 1911,
-            now_.month,
-            now_.day,
+            now.year - 1911,
+            now.month,
+            now.day,
         )
 
         params = {
             'menuCode': 'RECVQRY',
             'detail': 'show',
             'recvname': '',
-            'doc_word': '',
-            'doc_no': '',
-            'doc_title': '',
-            'full_compare': '0',
-            'senderid': '',
-            'sendername': '',
-            'query_time': 'create_time',
-            'startDate': now_str,
-            'sHour': '00',
-            'endDate': now_str,
-            'eHour': '23',
-            'noteonly': '2',
-            'pn': '0',
-            'ifquery_recvqry': '1',
+            'doc_word': source_word,
+            'doc_no': source_number,
+            'doc_title': subject,
+            'full_compare': full_compare,
+            'senderid': source_id,
+            'sendername': source,
+            'query_time': query_time,
+            'startDate': start_date or now_str,
+            'sHour': start_hour,
+            'endDate': end_date or now_str,
+            'eHour': end_hour,
+            'noteonly': 2,
+            'pn': 0,
+            'ifquery_recvqry': 1,
             'full_compare_send': '',
         }
-        params.update(kwargs)
         r = self.get('webeClient/main.php', params=params)
 
         soup = bs4.BeautifulSoup(r.content, 'html.parser')
@@ -133,30 +128,151 @@ class eClient(requests.Session):
             tds = tr0.find_all('td')
 
             document = Document(
-                eclient=self,
+                id_=int(urlparse.parse_qs(urlparse.urlparse(tr0['linkto']).query)['dilistid'][0]),
                 checked='checked' in tds[1].input,
                 source=tds[4].contents[2].string,
-                source_word=tds[5].string,
-                source_no=int(tds[6].contents[1].string),
-                date=datetime.datetime.strptime(tds[8].string.split(' ')[0], '%Y/%m/%d'),
+                source_no=u'{:s}字第{:d}號'.format(tds[5].string, int(tds[6].contents[1].string)),
                 receiver=tds[9].contents[2].string,
+                receive_datetime=datetime.datetime.strptime(tds[8].string.split(' ')[0], '%Y/%m/%d'),
+                paper_nm=0,  # TODO
                 subject=tr1.contents[1].contents[2].string.strip().split(u'：', 1)[1],
-                dilistid=int(urlparse.parse_qs(urlparse.urlparse(tr0['linkto']).query)['dilistid'][0]),
             )
             documents.append(document)
 
         return documents
 
+    def receive_detail(self, document):
+        params = {
+            'menuCode': 'RECVQRY',
+            'detail': 'showdetail',
+            'dilistid': document.dilistid,
+            'listid': '',
+        }
 
-class Config(object):
-    def __init__(self):
-        pass
-        
+        r = self.get('webeClient/main.php', params=params)
+        soup = bs4.BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table', id='Table1')
+        trs = table.find_all('tr')
+
+        tds = trs[1].find_all('td')
+        document.paper_nm = tds[3].string
+
+        as_ = trs[4].find_all('a')
+        document.attachments = {}
+        for a_ in as_[1:]:
+            document.attachments[a_.string] = a_['href']
+
+        return document
+
+
+class Converter(object):
+    @staticmethod
+    def code(item):
+        return u'{item.code_no} {item.code_nm}'.format(item=item)
+
+    def __init__(self,
+                 connection):
+
+        self.connection = connection
+        self.conductors = connection.select(
+            from_='conductor',
+            fields=[Connection.rtrim('user_nm')],
+        )
+        self.secrets = connection.select(
+            from_='secret',
+            fields=[
+                Connection.rtrim('code_no'),
+                Connection.rtrim('code_nm'),
+            ],
+        )
+        self.papers = connection.select(
+            from_='paper',
+            fields=[
+                Connection.rtrim('code_no'),
+                Connection.rtrim('code_nm'),
+            ],
+        )
+        self.books = connection.select(
+            from_='book',
+            fields=[
+                Connection.rtrim('code_no'),
+                Connection.rtrim('code_nm'),
+            ],
+        )
+
+        secret_default = self.secrets[secrets.code_no == '1'].iloc[0]
+        paper_default = self.papers[papers.code_no == '21'].iloc[0]
+        book_default = self.books[books.code_no == '1'].iloc[0]
+
+        self.archive_default = pd.DataFrame([{
+            'secret': Converter.code(secret_default),
+            'paper': Converter.code(paper_default),
+            'book': Converter.code(book_default),
+            'speed': u'普通件',
+            'archive_no': 1,
+            'process_days': 0.5,
+            'measure': u'頁',
+            'ymm_user': u'系統管理員',
+        }])
+
+    def to_archives(documents):
+        archives = self.connection.select(
+            from_='archive',
+            fields=['sno', 'convert(int, receive_no)'],
+            wheres=['Year(receive_date) = {:d}'.format(now.year)],
+            order_bys=['sno desc'],
+        )
+
+        now = datetime.datetime.now()
+        if archives.empty:
+            sno = 1
+            receive_no = 1
+        else:
+            sno = archives.sno.max() + 1
+            receive_no = archives.receive_no.max() + 1
+
+        new_archives = []
+        for (num, document) in enumerate(documents):
+            secret = self.secrets[self.secrets.code_nm == documents.secret_nm].iloc[0]
+            paper = self.papers[self.papers.code_nm == documents.paper_nm].iloc[0]
+            book = self.books[self.books.code_nm == documents.book_nm].iloc[0]
+
+            archive = self.archive_default.copy()
+            archive.update([{
+                'ymm_year': now.year,
+                'sno': sno + num,
+                'receive_no': receive_no + num,
+                'receive_date': '{:%Y/%m/%d}'.format(now),
+                'secret': Converter.code(secret),
+                'source': document.source,
+                'source_no': document.source_no,
+                'paper': Converter.code(paper),
+                'book': Converter.code(book),
+                'user_nm': document.user_nm,
+                'subject': document.subject,
+                'ymm_month': now.month,
+            }])
+            new_archives.append(archive)
+
+        new_archives = pd.concat(new_archives, ignore_index=True)
+        return new_archives
+
 
 class Connection(pypyodbc.Connection):
-    def select(self, 
+    @staticmethod
+    def rtrim(field):
+        return 'rtrim(' + field + ')'
+
+    @staticmethod
+    def sentence(strs, sep=',', begin='', end='', default=''):
+        begin = begin + ' '
+        sep = ' ' + sep + ' '
+        end = ' ' + end
+        return (begin + sep.join(strs) + end) if strs else default
+
+    def select(self,
                from_,
-               top=None, 
+               top=None,
                fields=['*'],
                wheres=[],
                order_bys=[]):
@@ -167,37 +283,45 @@ class Connection(pypyodbc.Connection):
             u'{where} '
             u'{order_by} '
         ).format(
-            top='' if top is None else 'top {} '.format(top), 
-            field=', '.join(fields),
+            top='' if top is None else 'top {} '.format(top),
+            field=Connection.setence(fields),
             from_=from_,
-            where='where ' + ' and '.join(wheres) if wheres else '',
-            order_by='order by ' + ', '.join(order_bys) if order_bys else '',
+            where=Connection.sentence(wheres, sep='and', begin='where'),
+            order_by=Connection.sentence(order_bys, bein='order by'),
         )
 
         print(query)
 
-        df = pd.read_sql(query, con=self, coerce_float=True)
+        df = pd.read_sql(query, con=self)
         return df
 
+    '''
     def insert(self,
                into,
                fields=[],
                values=[]):
 
+        values = ['\'' + unicode(value) + '\'' for value in values]
+
         query = (
             u'insert '
             u'into {into} {field} '
             u'values {value} '
-        ).format( 
+        ).format(
             into=into,
-            field='(' + ', '.join(fields) + ')' if fields else '',
-            value='(' + ', '.join(map(u'\'{}\''.format, values)) + ')' if values else '',
+            field=Connection.sentence(fields, begin='(', end=')'),
+            value=Connection.sentence(values, begin='(', end=')'),
         )
 
         print(query)
-        '''
         cursor = self.cursor()
         cursor.execute(query)
         cursor.close()
         self.commit()
-        '''
+    '''
+    def insert(self,
+               df,
+               into):
+
+        df.to_sql(into, con=self, if_exists='append', index=False)
+
